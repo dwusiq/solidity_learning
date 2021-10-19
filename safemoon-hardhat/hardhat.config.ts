ced9ad2987878c262529605b8daeff40282c4ee2
dwusiq <dwusiq@163.com>
2021-10-19 16:35:26 +0800

    add:加上uniswap的代码及测试案例

--- safemoon-hardhat/hardhat.config.ts
+++ safemoon-hardhat/hardhat.config.ts
@@ -1,10 +1,45 @@
-import {HardhatUserConfig} from 'hardhat/types';
 import 'hardhat-deploy';
 import 'hardhat-deploy-ethers';
+import { HardhatUserConfig } from 'hardhat/types';
 
 const config: HardhatUserConfig = {
   solidity: {
-    version: '0.6.12', // 支持solidity版本
+    compilers: [
+      {
+        version: '0.6.12', // 支持solidity版本
+      }],
+    overrides: {
+      "contracts/uniswapForTest/BEP20USDT.sol": {
+        version: "0.5.16",
+        settings: {
+        }
+      },
+      "contracts/uniswapForTest/WBNB.sol": {
+        version: "0.4.18",
+        settings: {
+        }
+      },
+      "contracts/uniswapForTest/UniswapV2Factory.sol": {
+        version: "0.5.16",
+        settings: {
+          evmVersion: "istanbul",
+          optimizer: {
+            enabled: true,
+            runs: 200
+          }
+        }
+      },
+      "contracts/uniswapForTest/UniswapV2Router02.sol": {
+        version: "0.6.6",
+        settings: {
+          evmVersion: "istanbul",
+          optimizer: {
+            enabled: true,
+            runs: 200
+          }
+        }
+      }
+    }
   },
   // namedAccounts: {
   //   deployer: 0,
---
+++ safemoon-hardhat/test/SafeMoon.ts
@@ -0,0 +1,49 @@
+const { expect } = require("chai");
+const { ethers, upgrades } = require("hardhat");
+
+
+
+
+describe("SafeMoon Test", function () {
+    const decimalUsdt = 18;
+    const decimalEth = 18;
+
+    let usdtAddr: any;
+    let wbnbAddr: any;
+    let uniswapV2FactoryAddr: any;
+    let uniswapV2Router02Addr: any;
+    let deployer:any, user0, user1, user2, user3, user4;
+
+    it("init params", async function () {
+        [deployer, user0, user1, user2, user3, user4] = await ethers.getSigners();
+    });
+
+    it("deploy all contract", async function () {
+        const ftryUsdt = await ethers.getContractFactory("BEP20USDT");
+        const usdt = await ftryUsdt.deploy();
+        await usdt.deployed();
+        usdtAddr = usdt.address;
+
+        const ftryWBNB = await ethers.getContractFactory("WBNB");
+        const wbnb = await ftryWBNB.deploy();
+        await wbnb.deployed();
+        wbnbAddr = wbnb.address;
+
+        const ftryUniswapV2Factory = await ethers.getContractFactory("UniswapV2Factory");
+        const uniswapV2Factory = await ftryUniswapV2Factory.deploy(wbnb.address);
+        await uniswapV2Factory.deployed();
+        uniswapV2FactoryAddr = uniswapV2Factory.address;
+
+        const ftryUniswapV2Router02 = await ethers.getContractFactory("UniswapV2Router02");
+        const uniswapV2Router02 = await ftryUniswapV2Router02.deploy(uniswapV2Factory.address, wbnb.address);
+        await uniswapV2Router02.deployed();
+        uniswapV2Router02Addr = uniswapV2Router02.address;
+
+        //log
+        console.log("deployer:%s", deployer.address);
+        console.log("usdtAddr:%s", usdtAddr);
+        console.log("wbnbAddr:%s", wbnbAddr);
+        console.log("uniswapV2FactoryAddr:%s", uniswapV2FactoryAddr);
+        console.log("uniswapV2Router02Addr:%s", uniswapV2Router02Addr);
+    });
+});
