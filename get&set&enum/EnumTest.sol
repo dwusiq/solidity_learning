138dd80e4d45d4b8f0f6f504348cb352b20a640c
dwusiq <dwusiq@163.com>
2021-10-19 14:52:07 +0800

    remove: 移除历史的solidity文件，因为已迁移

--- get&set&enum/EnumTest.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity ^0.7.0;
-
-contract EnumTest{
-    enum MyEnum{One,Tow,Tree,Four}
-    function get()pure public returns(uint value){
-        return uint(MyEnum.Tree);
-    }
-}
--- interface/Ibox.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity ^0.4.25;
-
-//定义一个接口
-interface Ibox{
-    function add(string name)external;//往盒子添加物品，重复执行会覆盖旧值。 name:物品名称
-    function get()external constant returns(string);//返回盒子存放的东西的名称
-}
